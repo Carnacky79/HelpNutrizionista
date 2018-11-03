0212862c2c9253cbbc567580b7e62a61d811f8cc
Francesco Lombardi <francesco.lombardi@pianetasoft.com>
2018-11-03 20:39:45 +0100

    Aggiunta connessione a foglio Excel e popolamento DataGridView

--- HelpNutrizionista/Form1.cs
+++ HelpNutrizionista/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,26 @@ namespace HelpNutrizionista
 {
     public partial class Form1 : Form
     {
+        private static OleDbConnection conn = new OleDbConnection("provider = Microsoft.Jet.OLEDB.4.0; Data Source = 'C:\\Users\\carna\\Desktop\\tabella_alimenti.xls'; Extended properties = Excel 8.0");
+        private static DataSet dt = new DataSet();
+        private static OleDbDataAdapter command;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fillDataset();
+        }
+
+        private void fillDataset()
+        {
+            command = new OleDbDataAdapter("Select * From [Foglio1$]", conn);
+            command.TableMappings.Add("Table", "TestTable");
+            command.Fill(dt);
+            dataGridView1.DataSource = dt.Tables[0];
+            conn.Close();
         }
     }
 }
