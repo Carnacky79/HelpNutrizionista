5ef2be992c4b9470f2fa245b2e95c20fcd542099
Francesco Lombardi <francesco.lombardi@pianetasoft.com>
2018-11-05 02:20:26 +0100

    Aggiunto bottone di inserimento TODO: farlo meglio

--- HelpNutrizionista/Form1.Designer.cs
+++ HelpNutrizionista/Form1.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             this.cbAlimenti1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnAggiungi = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
@@ -40,13 +45,56 @@
             this.cbAlimenti1.Size = new System.Drawing.Size(400, 21);
             this.cbAlimenti1.TabIndex = 0;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(40, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Qtà (g)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Edibile (g)";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(69, 62);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(40, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // btnAggiungi
+            // 
+            this.btnAggiungi.Location = new System.Drawing.Point(13, 97);
+            this.btnAggiungi.Name = "btnAggiungi";
+            this.btnAggiungi.Size = new System.Drawing.Size(59, 28);
+            this.btnAggiungi.TabIndex = 6;
+            this.btnAggiungi.Text = "Aggiungi";
+            this.btnAggiungi.UseVisualStyleBackColor = true;
+            this.btnAggiungi.Click += new System.EventHandler(this.btnAggiungi_Click);
+            // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(502, 13);
+            this.listBox1.Location = new System.Drawing.Point(509, 13);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(500, 290);
-            this.listBox1.TabIndex = 1;
+            this.listBox1.Size = new System.Drawing.Size(487, 95);
+            this.listBox1.TabIndex = 7;
             // 
             // Form1
             // 
@@ -54,17 +102,28 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1008, 729);
             this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnAggiungi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.cbAlimenti1);
             this.Name = "Form1";
             this.Text = "Aiuto Nutrizionista";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ComboBox cbAlimenti1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnAggiungi;
         private System.Windows.Forms.ListBox listBox1;
     }
 }
--- HelpNutrizionista/Form1.cs
+++ HelpNutrizionista/Form1.cs
@@ -13,10 +13,10 @@ namespace HelpNutrizionista
 {
     public partial class Form1 : Form
     {
-        private static OleDbConnection conn = new OleDbConnection("provider = Microsoft.Jet.OLEDB.4.0; Data Source = 'C:\\Users\\carna\\Desktop\\tabella_alimenti.xls'; Extended properties = Excel 8.0");
-        private static DataSet dt = new DataSet();
-        private static DataTable ds = new DataTable();
-        private static OleDbDataAdapter command;
+        private static OleDbConnection conn = new OleDbConnection("provider = Microsoft.Jet.OLEDB.4.0; Data Source = 'C:\\Users\\carna\\Desktop\\tabella_alimenti.xls'; Extended properties =\"Excel 8.0;HDR=YES;\"");
+        private static DataSet ds = new DataSet();
+        private static DataTable dt = new DataTable();
+        private static OleDbDataAdapter dataAdapt;
         public Form1()
         {
             InitializeComponent();
@@ -29,17 +29,38 @@ namespace HelpNutrizionista
 
         private void fillCombobox(ComboBox cb)
         {
-            command = new OleDbDataAdapter("Select * From [Foglio1$]", conn);
-            command.TableMappings.Add("Table", "Alimenti");
-            command.Fill(ds);
+            dataAdapt = new OleDbDataAdapter("Select * From [Foglio4$]", conn);
+            dataAdapt.TableMappings.Add("Table", "Alimenti");
+            dataAdapt.Fill(ds);
 
-            cb.Text = "--Seleziona un Alimento--";
-
-            cb.DataSource = ds;
+            cb.DataSource = ds.Tables[0];
             cb.DisplayMember = "Descrizione";
             cb.ValueMember = "ID";
 
+            cb.Text = "--Seleziona un Alimento--";
+
             conn.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private float PerCent(float ingresso, float molti)
+        {
+            return (ingresso / 100) * molti;
+        }
+
+        private void btnAggiungi_Click(object sender, EventArgs e)
+        {
+            string ID = cbAlimenti1.SelectedValue.ToString();
+
+            DataRow[] dr = ds.Tables[0].Select("ID = " + ID);
+
+            listBox1.Items.Add(dr[0][0].ToString() + " | " + dr[0][1].ToString() + " | " + dr[0][2].ToString() + " | ");
+
+            MessageBox.Show(dr[0][2].ToString());
         }
     }
 }
